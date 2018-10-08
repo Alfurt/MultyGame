a83128e5f5d2dd997f9ab6c8165e0f996fdcb287
Alfurt <1454alfurt@gmail.cm>
2018-10-09 00:11:02 +0300

    upd4
    add start final page
    check my code

--- scripts.js
+++ scripts.js
@@ -11,19 +11,31 @@ but.onclick = function()
     var ans = document.getElementById("answer").innerHTML.toLowerCase();
 
     var compare;
+    // -- Здесь все проверки --
     if(questNum == 1)
     {
       // Правильный ответ на первый вопрос
-      document.getElementById("quest").innerHTML  = "Ответ";
+      document.getElementById("quest").innerHTML = "Губка Боб Квадратные Штаны - главный герой американского одноимённого мультипликационного сериала премьера которого состоялась 1 мая 1999 года на канале Никэлодэон.";
       // Соответствующая картинка
-      document.getElementById("pic").src  = "https://citaty.info/files/characters/25972.jpg";
+      document.getElementById("pic").src = "https://pp.userapi.com/c850436/v850436663/1c123/wjVxpDCnRqM.jpg";
       // Правильные слова
-      compare = ["asd", "sss", "fff"];
+      compare = ["губка боб", "губка боб квадратные штаны", "спанч боб"];
     }
     else if(questNum == 2)
     {
       // 2
+      //document.getElementById("quest").innerHTML = "";
+      //document.getElementById("pic").src = "";
+      //compare = ["", ""];
     }
+    else if(questNum == 3)
+    {
+      // и так далее...
+      //document.getElementById("quest").innerHTML = "Текст ответа";
+      //document.getElementById("pic").src = "Ссылка на картинку с ответом";
+      //compare = ["слово1", "слово2"]; // В нижнем регистре
+    }
+    // -- Конец questNum == 8 --
 
     var b = false;
     for(var i in compare)
@@ -40,7 +52,7 @@ but.onclick = function()
     }
     else
     {
-      document.getElementById("title").innerHTML = "Не правильно...";
+      document.getElementById("title").innerHTML = "Неправильно...";
       document.getElementById("answer").classList.add("ansWrong");
       document.getElementById("but").classList.add("btnWrong");
     }
@@ -57,11 +69,30 @@ but.onclick = function()
     document.getElementById("but").classList.remove("btnRight");
     document.getElementById("but").classList.remove("btnWrong");
 
-    if(questNum = 2)
+    // -- Здесь подготовки всех вопросов
+    if(questNum == 2)
     {
-      document.getElementById("quest").innerHTML  = "Ответ";
-      document.getElementById("pic").src  = "https://citaty.info/files/characters/25972.jpg";
-
+      document.getElementById("quest").innerHTML = "Из какого сериала взялся этот паренек?";
+      document.getElementById("pic").src = "https://pp.userapi.com/c850436/v850436663/1c0b2/vfBX72RmZlg.jpg";
+    }
+    //else if(questNum == 3)
+    //{
+      // 3
+      //  document.getElementById("quest").innerHTML = "Текст вопроса";
+      // document.getElementById("pic").src = "Картинка для вопроса";
+    //}
+    //else if(questNum == 4)
+    //{
+      // до questNum = 8
+    //}
+    else
+    {
+      // Финал
+      document.getElementById("title").innerHTML = "Результат";
+      document.getElementById("quest").innerHTML = "Правильно: " + 100*(rightAns/8) + "%";
+      document.getElementById("pic").src = "";
+      //document.getElementById("answer").display = "none";
+      //document.getElementById("but").display = "none";
     }
   }
 }
